4595ca604abe52e01e9b01e8d20f1122fdb3d818
rfancn <reg_info@qq.com>
2025-11-17 17:11:50 +0800

    debug

--- g/config.go
+++ g/config.go
@@ -10,28 +10,28 @@ type confRoot struct {
 
 // Config 根配置结构
 type confApp struct {
-	FileWatch confFileWatch `toml:"watcher"`
-	Sync      confSync      `toml:"sync"`
-	Event     confEvent     `toml:"event"`
+	FileWatch confFileWatch `mapstructure:"watcher"`
+	Sync      confSync      `mapstructure:"sync"`
+	Event     confEvent     `mapstructure:"event"`
 }
 
 // confFileWatch 监控配置
 type confFileWatch struct {
-	Paths        []string      `toml:"paths"`
-	DebounceTime time.Duration `toml:"debounce_time"`
+	Paths        []string      `mapstructure:"paths"`
+	DebounceTime time.Duration `mapstructure:"debounce_time"`
 }
 
 // confSync 同步配置
 type confSync struct {
-	Endpoint     string `toml:"endpoint"`
-	RemotePath   string `toml:"remote_path"`
-	AccessKey    string `toml:"access_key"`
-	AccessSecret string `toml:"access_secret"`
-	acl          string `toml:"private"`
+	Endpoint     string `mapstructure:"endpoint"`
+	RemotePath   string `mapstructure:"remote_path"`
+	AccessKey    string `mapstructure:"access_key"`
+	AccessSecret string `mapstructure:"access_secret"`
+	acl          string `mapstructure:"private"`
 }
 
 type confEvent struct {
-	Url    string `toml:"url"`
-	Port   int    `toml:"port"`
-	Secret string `toml:"secret"`
+	Url    string `mapstructure:"url"`
+	Port   int    `mapstructure:"port"`
+	Secret string `mapstructure:"secret"`
 }
